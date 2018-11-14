94293bdce8552f6c4259ea8dc792f458b3808868
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-14 11:42:13 +0530

    Gravity and Collision behavior

--- UIKitDynamics/ViewController.swift
+++ UIKitDynamics/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    
+    var collision: UICollisionBehavior!
+    
+    
+    // MARK: App LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        square.backgroundColor = UIColor.gray
+        view.addSubview(square)
+        
+        let barrier = UIView(frame: CGRect(x: 0, y: 300, width: 130, height: 20))
+        barrier.backgroundColor = UIColor.red
+        view.addSubview(barrier)
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [square])
+        animator.addBehavior(gravity)
+        
+        // collision = UICollisionBehavior(items: [square,barrier])
+        collision = UICollisionBehavior(items: [square])
+        // add a boundary that has the same frame as the barrier
+        collision.addBoundary(withIdentifier: "banner" as NSCopying, for: UIBezierPath(rect: barrier.frame))
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
     }
-
-
 }
 
